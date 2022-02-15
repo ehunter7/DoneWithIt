3803202bc51f2e250f13dc7adb3d8c43f0b50ad1
Eric Hunter <ehunter7@live.com>
2022-02-14 20:29:31 -0800

    add error handling

--- Backend/routes/my.js
+++ Backend/routes/my.js
@@ -5,7 +5,7 @@ const listingsStore = require("../store/listings");
 const auth = require("../middleware/auth");
 const listingMapper = require("../mappers/listings");
 
-router.get("/listings", (req, res) => {
+router.get("/listings", auth, (req, res) => {
   const listings = listingsStore.filterListings(
     (listing) => listing.userId === req.user.userId
   );
